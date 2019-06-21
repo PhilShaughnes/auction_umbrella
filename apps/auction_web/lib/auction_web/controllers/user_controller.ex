6e0eb19a77ad2f6b2d79a22194715ac4dfeab9de
Phil <Philip.Shaughnessy@gmail.com>
2019-06-20 20:08:31 -0400

    add user controller

--- apps/auction_web/lib/auction_web/controllers/user_controller.ex
+++ apps/auction_web/lib/auction_web/controllers/user_controller.ex
@@ -1,1 +1,18 @@
+defmodule AuctionWeb.UserController do
+  use AuctionWeb, :controller
 
+  def show(conn, %{"id" => id}) do
+    render conn, "show.html", user: Auction.get_user(id)
+  end
+
+  def new(conn, _paraams) do
+    render conn, "new.html", user: Auction.new_user()
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Auction.insert_user(user_params) do
+      {:ok, user} -> redirect conn, to: Routes.user_path(conn, :show, user)
+      {:error, user} -> render conn, "new.html", user: user
+    end
+  end
+end
